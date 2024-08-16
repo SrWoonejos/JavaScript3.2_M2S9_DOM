0d107b31ea3e921e0034bc9fb26d5c8c6f991bce
SrWoonejos <danmirandam@gmail.com>
2024-08-16 18:49:07 -0400

    update

---
+++ Assets/script.js
@@ -0,0 +1,20 @@
+function mostrarText2() {
+    document.getElementById("text2").style.display = "block";
+}
+
+function ocultarText2() {
+    document.getElementById("text2").style.display = "none";
+}
+
+function agrandarImagen() {
+    document.getElementById("img").style.width = "40%";
+}
+
+function reducirImagen() {
+    document.getElementById("img").style.width = "20%";
+}
+
+function agrandarTexto() {
+    document.getElementById("caja3").style.fontSize = "2em";
+}
+
